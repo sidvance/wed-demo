0e2b5c80794199f55d520ee703f15cd4a379cad7
Cassidy <cassidy.vance@hotmail.com>
2023-03-01 14:05:46 -0800

    demo without changes

---
+++ varsAndConditionals.js
@@ -0,0 +1,16 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Linda and
+    Makenzie.
+*/ 
+
+let makAttack = 50
+let linAttack = 50
+
+if (makAttack > linAttack){
+    console.log("My money's on Makenzie")
+} else if (makAttack < linAttack){
+    console.log("My money's on mom")
+} else {
+    console.log("Why are they being so dramatic")
+}
